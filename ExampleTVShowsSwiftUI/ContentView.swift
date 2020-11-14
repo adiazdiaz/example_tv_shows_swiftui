a883ad4f2880731cc655bca3cda47dc5d31d27e3
Alberto Díaz <adiazdi@gmail.com>
2020-11-14 12:10:05 +0100

    Initial Commit

---
+++ ExampleTVShowsSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ExampleTVShowsSwiftUI
+//
+//  Created by Alberto Díaz Díaz on 14/11/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ExampleTVShowsSwiftUI/ExampleTVShowsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleTVShowsSwiftUIApp.swift
+//  ExampleTVShowsSwiftUI
+//
+//  Created by Alberto Díaz Díaz on 14/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct ExampleTVShowsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
